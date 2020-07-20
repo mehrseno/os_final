15fc922f1d5ae0f03ec194739af13a0c9254c411
mehrseno <m.seno@aut.ac.ir>
2020-07-20 22:30:33 +0430

    ticketlock.c and ticketlock.h changed

---
+++ ticketlock.c
@@ -0,0 +1,20 @@
+#include "types.h"
+#include "defs.h"
+#include "param.h"
+#include "x86.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+#include "ticketlock.h"
+
+void initTicketLock(struct ticketlock *lk, char *name)
+{
+}
+
+void acquireTicketLock(struct ticketlock *lk)
+{
+}
+
+void releaseTicketLock(struct ticketlock *lk)
+{
+} 
